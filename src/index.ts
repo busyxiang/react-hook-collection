68346643d7a06a68a3d9694c24150bdd6b37ec3d
Ray Foo <ray@6biz.ai>
2022-04-05 22:31:15 +0800

    Fix did not export useNetworkStatus hook

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import useMounted from './useMounted';
 import useLocalStorage from './useLocalStorage';
 import useInjectScript from './useInjectScript';
+import useNetworkStatus from './useNetworkStatus';
 
 export * from './useGoogleDrivePicker';
 
-export { useMounted, useLocalStorage, useInjectScript };
+export { useMounted, useLocalStorage, useInjectScript, useNetworkStatus };
